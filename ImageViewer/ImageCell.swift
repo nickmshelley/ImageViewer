da8b93f8abd45ee3d1b271fab862e8f572807ea9
Nick Shelley <nickmshelley@gmail.com>
2019-04-17 18:05:05 -0600

    Get collection view up.

---
+++ ImageViewer/ImageCell.swift
@@ -0,0 +1,9 @@
+//
+//  ImageCell.swift
+//  ImageViewer
+//
+//  Created by Heather Shelley on 4/17/19.
+//  Copyright © 2019 Mine. All rights reserved.
+//
+
+import Foundation
--- ImageViewer/ImagesViewController.swift
+++ ImageViewer/ImagesViewController.swift
@@ -3,15 +3,52 @@ import UIKit
 
 class ImagesViewController: UIViewController {
     var images: [USAImage] = []
+    lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 20, left: 10, bottom: 10, right: 10)
+        layout.minimumInteritemSpacing = 12
+        layout.minimumLineSpacing = 12
+        layout.itemSize = CGSize(width: 120, height: 120)
+        return UICollectionView(frame: .zero, collectionViewLayout: layout)
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .white
+        collectionView.backgroundColor = .green
+        view.addSubview(collectionView)
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "Cell")
+        
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+            ])
         
         NetworkInteractor.fetchImages() { images in
-            self.images = images
-            print(images)
+            DispatchQueue.main.async {
+                self.images = images
+                self.collectionView.reloadData()
+            }
         }
     }
+}
+
+extension ImagesViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return images.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath)
+        cell.backgroundColor = .blue
+        return cell
+    }
+    
+    
 }
